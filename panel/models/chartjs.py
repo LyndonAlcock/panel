568c2be08d9095236e4203d5fdc299ff68a3cb4f
LyndonAlcock <86410002+LyndonAlcock@users.noreply.github.com>
2024-10-15 15:21:04 +0100

    code practically added unmodified from the doc

---
+++ panel/models/chartjs.py
@@ -0,0 +1,8 @@
+from bokeh.core.properties import Int, String
+from .layout import HTMLBox
+
+class ChartJS(HTMLBox):
+    """Custom ChartJS Model"""
+
+    object = String()
+    clicks = Int()
---
+++ panel/tests/pane/test_chartjs.py
@@ -0,0 +1,15 @@
+import panel as pn
+
+
+def test_constructor():
+    chartjs = pn.pane.ChartJS(object="Click Me Now!")
+
+def get_app():
+    chartjs = pn.pane.ChartJS(object="Click Me Now!")
+    return pn.Column(
+        chartjs, pn.Param(chartjs, parameters=["object", "clicks"])
+    )
+
+pn.config.js_files["chartjs"]="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+get_app().show()
+
